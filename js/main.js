a7de1943dbb75783977ffee4e378540cc850ed09
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-06-29 19:24:32 +0200

    exercise done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-const firstName = prompt("Inserisci il nome")
-const lastName = prompt("Inserisci il cognome")
-const favColor = prompt("Inserisci il tuo colore preferito")
+const firstName = prompt("Inserisci il nome");
+const lastName = prompt("Inserisci il cognome");
+const favColor = prompt("Inserisci il tuo colore preferito");
+const age = prompt("Inserisci la tua etá");
 
+document.getElementById("hello-title").innerHTML = `Ciao ${firstName}, ecco la tua nuova password`;
+document.getElementById("name").innerHTML = firstName;
+document.getElementById("lastname").innerHTML = lastName;
+document.getElementById("color").innerHTML = favColor;
+document.getElementById("age").innerHTML = age;
+document.getElementById("password").innerHTML = firstName + lastName + favColor + "21";
